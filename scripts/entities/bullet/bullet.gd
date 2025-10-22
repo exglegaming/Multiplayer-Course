dfc86191113e386737b1184fd5ef0668d88400d3
ExgleGaming <EaglesFan91x@outlook.com>
2025-10-21 23:39:53 -0400

    hurtbox and hitbox components

--- scripts/entities/bullet/bullet.gd
+++ scripts/entities/bullet/bullet.gd
@@ -6,9 +6,11 @@ const SPEED: int = 600
 var direction: Vector2
 
 @onready var life_timer: Timer = $LifeTimer
+@onready var hitbox_component: HitboxComponent = $HitboxComponent
 
 
 func _ready() -> void:
+    hitbox_component.hit_hurtbox.connect(_on_hit_hurtbox)
     life_timer.timeout.connect(_on_life_timer_timeout)
 
 
@@ -28,3 +30,7 @@ func register_collision() -> void:
 func _on_life_timer_timeout() -> void:
     if is_multiplayer_authority():
         queue_free()
+
+
+func _on_hit_hurtbox(hurtbox_component: HurtboxComponent) -> void:
+    register_collision()
--- scripts/entities/enemy/enemy.gd
+++ scripts/entities/enemy/enemy.gd
@@ -3,65 +3,49 @@ extends CharacterBody2D
 
 var target_position: Vector2
 
-@onready var area_2d: Area2D = $Area2D
 @onready var target_acquisistion_timer: Timer = $TargetAcquisitionTimer
 @onready var health_component: HealthComponent = $HealthComponent
 
 
 func _ready() -> void:
-	area_2d.area_entered.connect(_on_area_entered)
-	target_acquisistion_timer.timeout.connect(_on_target_aqcuisition_timer_timeout)
-	
-	if is_multiplayer_authority():
-		health_component.died.connect(_on_died)
-		acquire_target()
+  target_acquisistion_timer.timeout.connect(_on_target_aqcuisition_timer_timeout)
+  
+  if is_multiplayer_authority():
+    health_component.died.connect(_on_died)
+    acquire_target()
 
 
 func _process(delta: float) -> void:
-	if is_multiplayer_authority():
-		velocity = global_position.direction_to(target_position) * 40
-		move_and_slide()
-
-
-func handle_hit() -> void:
-	health_component.damage(1)
+  if is_multiplayer_authority():
+    velocity = global_position.direction_to(target_position) * 40
+    move_and_slide()
 
 
 func acquire_target() -> void:
-	var players := get_tree().get_nodes_in_group("player")
-	var nearest_player: Player = null
-	var nearest_squared_distance: float
+  var players := get_tree().get_nodes_in_group("player")
+  var nearest_player: Player = null
+  var nearest_squared_distance: float
 
-	for player in players:
-		if nearest_player == null:
-			nearest_player = player
-			nearest_squared_distance = nearest_player.global_position.distance_squared_to(global_position)
-			continue
-		
-		var player_sqaured_distance: float = player.global_position.distance_squared_to(global_position)
-		if player_sqaured_distance < nearest_squared_distance:
-			nearest_squared_distance = player_sqaured_distance
-			nearest_player = player
-	
-	if nearest_player != null:
-		target_position = nearest_player.global_position
-
-
-func _on_area_entered(other_area: Area2D) -> void:
-	if !is_multiplayer_authority():
-		return
-
-	if other_area.owner is Bullet:
-		var bullet := other_area.owner as Bullet
-		bullet.register_collision()
-		handle_hit()
+  for player in players:
+    if nearest_player == null:
+      nearest_player = player
+      nearest_squared_distance = nearest_player.global_position.distance_squared_to(global_position)
+      continue
+    
+    var player_sqaured_distance: float = player.global_position.distance_squared_to(global_position)
+    if player_sqaured_distance < nearest_squared_distance:
+      nearest_squared_distance = player_sqaured_distance
+      nearest_player = player
+  
+  if nearest_player != null:
+    target_position = nearest_player.global_position
 
 
 func _on_target_aqcuisition_timer_timeout() -> void:
-	if is_multiplayer_authority():
-		acquire_target()
+  if is_multiplayer_authority():
+    acquire_target()
 
 
 func _on_died() -> void:
-	GameEvents.emit_enemy_died()
-	queue_free()
+  GameEvents.emit_enemy_died()
+  queue_free()
