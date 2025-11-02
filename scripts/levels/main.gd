73557e4bd0e95dfe5b3e3ce7ab4469dc881c8123
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-01 23:09:22 -0400

    handling game over

--- scripts/levels/main.gd
+++ scripts/levels/main.gd
@@ -1,5 +1,8 @@
 extends Node
 
+
+const MAIN_MENU_SCENE: String = "res://scenes/ui/main_menu/main_menu.tscn"
+
 var player_scene: PackedScene = preload("uid://bvlv7g7jv37sh")
 var dead_peers: Array[int] = []
 
@@ -22,6 +25,7 @@ func _ready() -> void:
 
 	peer_ready.rpc_id(1)
 	enemy_manager.round_completed.connect(_on_round_completed)
+	multiplayer.server_disconnected.connect(_on_server_disconnected)
 
 
 @rpc("any_peer", "call_local", "reliable")
@@ -30,6 +34,8 @@ func peer_ready() -> void:
 	multiplayer_spawner.spawn({"peer_id": sender_id})
 	enemy_manager.sychronize(sender_id)
 
+	print(multiplayer.get_unique_id())
+
 
 func respawn_dead_peers() -> void:
 	for peer_id in dead_peers:
@@ -37,9 +43,33 @@ func respawn_dead_peers() -> void:
 	dead_peers.clear()
 
 
+func end_game() -> void:
+	multiplayer.multiplayer_peer = null
+	get_tree().change_scene_to_file(MAIN_MENU_SCENE)
+
+
+func check_game_over() -> void:
+	var is_game_over: bool = true
+	var all_peers: PackedInt32Array= multiplayer.get_peers()
+	all_peers.push_back(multiplayer.get_unique_id())
+
+	for peer_id in all_peers:
+		if !dead_peers.has(peer_id):
+			is_game_over = false
+			break
+	
+	if is_game_over:
+		end_game()
+
+
 func _on_player_died(peer_id: int) -> void:
 	dead_peers.append(peer_id)
+	check_game_over()
 
 
 func _on_round_completed() -> void:
 	respawn_dead_peers()
+
+
+func _on_server_disconnected() -> void:
+	end_game()
