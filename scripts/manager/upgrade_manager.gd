ff952214513eff0210609704da4c0c1a52f86e10
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-08 16:26:51 -0500

    allow clients to select upgrades

--- scripts/manager/upgrade_manager.gd
+++ scripts/manager/upgrade_manager.gd
@@ -7,6 +7,7 @@ extends Node
 @export var available_upgrades: Array[UpgradeResource]
 
 var upgrade_option_scene: PackedScene = preload("uid://c5crefykwsii7")
+var  peer_id_to_upgrade_opotions: Dictionary[int, Array] = {}
 
 
 func _ready() -> void:
@@ -14,10 +15,23 @@ func _ready() -> void:
 
 
 func generate_upgrade_options() -> void:
+	peer_id_to_upgrade_opotions.clear()
+
 	var connected_peer_ids: PackedInt32Array = multiplayer.get_peers()
 	connected_peer_ids.append(MultiplayerPeer.TARGET_PEER_SERVER)
 	for connected_peer_id in connected_peer_ids:
-		var selected_upgrades: Array = [available_upgrades[0].id, available_upgrades[0].id, available_upgrades[0].id]
+		var selected_upgrades: Array = [
+			available_upgrades[0].id, 
+			available_upgrades[0].id, 
+			available_upgrades[0].id
+		]
+
+		peer_id_to_upgrade_opotions[connected_peer_id] = [
+			available_upgrades[0], 
+			available_upgrades[0], 
+			available_upgrades[0]
+		]
+
 		set_upgrade_options.rpc_id(connected_peer_id, selected_upgrades)
 
 
@@ -27,11 +41,28 @@ func show_upgrade_resources(upgrade_resources: Array[UpgradeResource]) -> void:
 
 	for i in range(upgrade_resources.size()):
 		var upgrade_option: UpgradeOption = upgrade_option_scene.instantiate()
-		upgrade_option.global_position = spawn_position.global_position
+		upgrade_option.set_upgrade_index(i)
+		upgrade_option.set_upgrade_resource(upgrade_resources[i])
 
+		upgrade_option.global_position = spawn_position.global_position
 		upgrade_option.global_position += Vector2.RIGHT * (initial_x + (x_difference * i))
 
 		spawn_root.add_child(upgrade_option)
+
+		upgrade_option.selected.connect(_on_upgrade_option_selected)
+
+
+@rpc("any_peer", "call_local", "reliable")
+func notify_upgrade_selected(upgrade_index: int) -> void:
+	if !is_multiplayer_authority():
+		return
+	
+	var peer_id: int = multiplayer.get_remote_sender_id()
+
+	print("Peer %s has selected upgrade with id %s" % [
+		peer_id, 
+		peer_id_to_upgrade_opotions[peer_id][upgrade_index].id
+	])
 
 
 @rpc("authority", "call_local", "reliable")
@@ -47,3 +78,7 @@ func set_upgrade_options(upgrades_ids: Array) -> void:
 
 func _on_round_completed() -> void:
 	generate_upgrade_options()
+
+
+func _on_upgrade_option_selected(upgrade_index: int) -> void:
+	notify_upgrade_selected.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER, upgrade_index)
