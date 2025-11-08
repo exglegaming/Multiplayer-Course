4fcd67803cb19109b0f7ea158e0546b51bca808a
ExgleGaming <EaglesFan91x@outlook.com>
2025-11-08 00:19:22 -0500

    ready up ui added part 1

--- scripts/manager/lobby_manager.gd
+++ scripts/manager/lobby_manager.gd
@@ -3,55 +3,73 @@ extends Node
 
 
 signal all_peers_ready
+signal self_peer_ready
+signal lobby_closed
 
 var ready_peer_ids: Array[int] = []
-var is_lobby_closed: bool
+var _is_lobby_closed: bool
+var is_lobby_closed: bool:
+	get:
+		return _is_lobby_closed
+	set(value):
+		_is_lobby_closed = value
+		if _is_lobby_closed:
+			lobby_closed.emit()
+
 
 
 func _ready() -> void:
-    if is_multiplayer_authority():
-        multiplayer.peer_disconnected.connect(_on_peer_disconnected)
-    
-    if multiplayer.multiplayer_peer is OfflineMultiplayerPeer:
-        all_peers_ready.emit.call_deferred()
+	if is_multiplayer_authority():
+		multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	
+	if multiplayer.multiplayer_peer is OfflineMultiplayerPeer:
+		all_peers_ready.emit.call_deferred()
 
 
 func _input(event: InputEvent) -> void:
-    if event.is_action_pressed("lobby_ready"):
-        request_peer_ready.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER)
-        get_viewport().set_input_as_handled()
+	if event.is_action_pressed("lobby_ready"):
+		request_peer_ready.rpc_id(MultiplayerPeer.TARGET_PEER_SERVER)
+		get_viewport().set_input_as_handled()
 
 
 func close_lobby() -> void:
-    is_lobby_closed = true
+	is_lobby_closed = true
+
+
+@rpc("authority", "call_local", "reliable")
+func set_peer_ready(peer_id: int) -> void:
+	if peer_id == multiplayer.get_unique_id():
+		self_peer_ready.emit()
+	
+	if !ready_peer_ids.has(peer_id):
+		ready_peer_ids.append(peer_id)
 
 
 @rpc("any_peer", "call_local", "reliable")
 func request_peer_ready() -> void:
-    if !is_multiplayer_authority() || is_lobby_closed:
-        return
-    
-    var sender_id: int = multiplayer.get_remote_sender_id()
-    if !ready_peer_ids.has(sender_id):
-        ready_peer_ids.append(sender_id)
-    
-    try_all_peers_ready()
+	if !is_multiplayer_authority() || is_lobby_closed:
+		return
+	
+	var sender_id: int = multiplayer.get_remote_sender_id()
+	set_peer_ready(sender_id)
+	
+	try_all_peers_ready()
 
 
 func try_all_peers_ready() -> void:
-    if check_all_peers_ready():
-        all_peers_ready.emit()
+	if check_all_peers_ready():
+		all_peers_ready.emit()
 
 
 func check_all_peers_ready() -> bool:
-    var all_peers: PackedInt32Array = multiplayer.get_peers()
-    all_peers.append(MultiplayerPeer.TARGET_PEER_SERVER)
+	var all_peers: PackedInt32Array = multiplayer.get_peers()
+	all_peers.append(MultiplayerPeer.TARGET_PEER_SERVER)
 
-    for peer_id in all_peers:
-        if !ready_peer_ids.has(peer_id):
-            return false
-    return true
+	for peer_id in all_peers:
+		if !ready_peer_ids.has(peer_id):
+			return false
+	return true
 
 
 func _on_peer_disconnected(_peer_id: int) -> void:
-    try_all_peers_ready()
+	try_all_peers_ready()
